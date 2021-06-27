5b2705f1a6fbe70b4327c7845c94e6d4206e6277
sillyhill-c <sahil.chatiwala@gmail.com>
2021-06-27 01:44:34 -0400

    Modified so that training does not occur each time the program is run using saved/loaded model

--- DrawingBoard.py
+++ DrawingBoard.py
@@ -57,23 +57,7 @@ testd = testd/255.0
 td = td.reshape(60000, 28, 28, 1)
 testd = testd.reshape(10000,28,28,1)
 
-network = Sequential([ #creates a sequential CNN with various conv, pooling, dropout, and dense layers
-    Conv2D(16, (3,3), activation = tf.nn.relu, input_shape= (28, 28, 1)),
-    Dropout(0.3),
-    Conv2D(32, (3,3), activation = tf.nn.relu),
-    MaxPooling2D(2,2),
-    Conv2D(32, (3,3,), activation = tf.nn.relu),
-    Dropout(0.3),
-    Conv2D(64,(3,3), activation = tf.nn.relu),
-    MaxPooling2D(2,2),
-    Flatten(),
-    Dense(100, activation = tf.nn.relu),
-    Dense(10, activation = tf.nn.softmax) #softmax activation due to the desired action of multiclass classification
-])
-
-network.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy',metrics = ['accuracy']) #compiles and starts learning of the network
-network.fit(td, tl, epochs = 20, callbacks = [CallbackTraining])
-network.evaluate(testd, testl) #evaluates the performance of the network using the MNIST test data
+network = tf.keras.models.load_model('DigitsNetwork')
 
 userHappy = True
 
